c02112ce1ae188dc2f9b6451573774ae9ea36b3f
siddiqui <siddiquiahs1@gmail.com>
2026-05-31 12:57:05 +0530

    Add Fee Structure section and refactor mobile layout

--- GoogleAppsScript.js
+++ GoogleAppsScript.js
@@ -1,24 +1,3 @@
-/**
- * Google Apps Script to handle school inquiry submissions
- * 
- * Instructions:
- * 1. Open Google Sheets (https://sheets.google.com) and create a new spreadsheet.
- * 2. In the first row, add the following headers in columns A through F:
- *    A: Timestamp | B: Parent Name | C: Phone Number | D: Child's Name | E: Class Applying For | F: Message
- * 3. Go to Extensions -> Apps Script.
- * 4. Replace the default code with this script.
- * 5. Click the "Save" icon.
- * 6. Click "Deploy" -> "New deployment".
- * 7. Click the "Select type" gear icon and choose "Web app".
- * 8. Set the configuration:
- *    - Description: TIPS Inquiry Form Handler
- *    - Execute as: Me (your email)
- *    - Who has access: Anyone (this is important, otherwise the website cannot post data here!)
- * 9. Click "Deploy". Authorize permissions if requested.
- * 10. Copy the "Web app" URL (ends with /exec).
- * 11. Paste this URL into the `SCRIPT_URL` variable at the top of your `assets/js/main.js` file!
- */
-
 function doPost(e) {
   try {
     // Parse the JSON payload sent by the frontend
